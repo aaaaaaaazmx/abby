71114ce7ea3b4c7396dbf25517796c6780d68c80
li <286440803@qq.com>
2022-10-24 18:06:12 +0800

    更改混淆规则

--- modules/modules_my/src/main/java/com/cl/modules_my/adapter/MyTroubleAdapter.kt
+++ modules/modules_my/src/main/java/com/cl/modules_my/adapter/MyTroubleAdapter.kt
@@ -1,20 +1,27 @@
 package com.cl.modules_my.adapter
 
+import androidx.databinding.DataBindingUtil
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.cl.common_base.util.calendar.Calendar
 import com.cl.modules_my.R
+import com.cl.modules_my.databinding.MyCalendarItemBinding
 import com.cl.modules_my.databinding.MyTroubleItemBinding
 import com.cl.modules_my.repository.MyTroubleData
 
 
 class MyTroubleAdapter(data: MutableList<MyTroubleData.Bean>?) :
-    BaseQuickAdapter<MyTroubleData.Bean, BaseDataBindingHolder<MyTroubleItemBinding>>(R.layout.my_trouble_item, data) {
+    BaseQuickAdapter<MyTroubleData.Bean, BaseViewHolder>(R.layout.my_trouble_item, data) {
 
-    override fun convert(
-        holder: BaseDataBindingHolder<MyTroubleItemBinding>,
-        item: MyTroubleData.Bean
-    ) {
-        holder.dataBinding?.apply {
+    override fun onItemViewHolderCreated(viewHolder: BaseViewHolder, viewType: Int) {
+        DataBindingUtil.bind<MyTroubleItemBinding>(viewHolder.itemView)
+    }
+
+    override fun convert(holder: BaseViewHolder, item: MyTroubleData.Bean) {
+        // 获取 Binding
+        val binding: MyTroubleItemBinding? = holder.getBinding()
+        binding?.apply {
             data = item
             executePendingBindings()
         }
