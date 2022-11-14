83a49bafd15f5c030a8f5f8fd7297dfa1b1c92e3
li <286440803@qq.com>
2022-11-14 10:27:53 +0800

    添加赎回页面

--- modules/modules_my/src/main/java/com/cl/modules_my/ui/ReDeemActivity.kt
+++ modules/modules_my/src/main/java/com/cl/modules_my/ui/ReDeemActivity.kt
@@ -1,9 +1,15 @@
 package com.cl.modules_my.ui
 
 import com.cl.common_base.base.BaseActivity
+import com.cl.common_base.widget.code.RedeemCodeInputView
+import com.cl.common_base.widget.code.VerificationCodeInputView
+import com.cl.common_base.widget.toast.ToastUtil
 import com.cl.modules_my.databinding.MyActivityRedeemBinding
 
-class ReDeemActivity: BaseActivity<MyActivityRedeemBinding>() {
+/**
+ *  赎回界面
+ */
+class ReDeemActivity: BaseActivity<MyActivityRedeemBinding>(), RedeemCodeInputView.OnInputListener {
     override fun initView() {
 
     }
@@ -12,5 +18,25 @@ class ReDeemActivity: BaseActivity<MyActivityRedeemBinding>() {
     }
 
     override fun initData() {
+    }
+
+    override fun onComplete(code: String?) {
+        code?.let {
+            if (it.isEmpty()) {
+                return@let
+            }
+            // 里面会包含一、需要去除
+            val split = it.split("一")
+            var codes = ""
+            split.forEach { string ->
+                codes+=string
+            }
+            // 打印
+            ToastUtil.shortShow(codes)
+            // todo 验证赎回code是否正确
+        }
+    }
+
+    override fun onInput() {
     }
 }
