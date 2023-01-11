5e70e59b936024caff80e6567cf071aafe0a2411
li <286440803@qq.com>
2023-01-11 11:01:00 +0800

    上架v1.1.4

--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -21,8 +21,8 @@ object Version {
     const val applicationId = "com.cl.abby"
     const val minSdk = 26
     const val targetSdk = 31
-    val versionCode = 13
-    const val versionName = "1.1.2"
+    val versionCode = 18
+    const val versionName = "1.1.4"
 
     const val coreKtxVersion = "1.8.0"
     const val appCompatVersion = "1.4.2"
