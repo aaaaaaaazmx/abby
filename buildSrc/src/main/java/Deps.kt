03aa05af955cbaf69b3d7c6c5e120e4c81285c9e
li <286440803@qq.com>
2023-09-05 17:42:47 +0800

    发布版本为V3.0.1

--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -16,8 +16,8 @@ object Version {
     const val applicationId = "com.cl.abby"
     const val minSdk = 26
     const val targetSdk = 33
-    val versionCode = 35
-    const val versionName = "3.0.0"
+    val versionCode = 36
+    const val versionName = "3.0.1"
 
     const val coreKtxVersion = "1.8.0"
     const val appCompatVersion = "1.4.2"
