36e0a8d7b600c06efcd963971c9d780de5102a0f
li <286440803@qq.com>
2023-09-27 16:24:21 +0800

    更新版本为v3.1.1,验证码6位改为4位。

--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -16,8 +16,8 @@ object Version {
     const val applicationId = "com.cl.abby"
     const val minSdk = 26
     const val targetSdk = 33
-    val versionCode = 41
-    const val versionName = "3.1.0"
+    val versionCode = 42
+    const val versionName = "3.1.1"
 
     const val coreKtxVersion = "1.8.0"
     const val appCompatVersion = "1.4.2"
