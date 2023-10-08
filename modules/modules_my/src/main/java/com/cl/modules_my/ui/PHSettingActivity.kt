fcb3d059335da40f6725a7b34c7c47efb03d5e75
li <286440803@qq.com>
2023-10-08 09:31:25 +0800

    添加PH配件设置界面

---
+++ modules/modules_my/src/main/java/com/cl/modules_my/ui/PHSettingActivity.kt
@@ -0,0 +1,19 @@
+package com.cl.modules_my.ui
+
+import com.cl.common_base.base.BaseActivity
+import com.cl.modules_my.databinding.MyPhSettingActivityBinding
+
+/**
+ * Ph笔数据设置界面
+ */
+class PHSettingActivity: BaseActivity<MyPhSettingActivityBinding>() {
+    override fun initView() {
+
+    }
+
+    override fun observe() {
+    }
+
+    override fun initData() {
+    }
+}
