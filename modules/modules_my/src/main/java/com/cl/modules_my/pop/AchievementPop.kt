9f659bd54a952e2eb5ed83731f3e46f17ec11797
li <286440803@qq.com>
2023-11-17 16:46:28 +0800

    添加3D旋转动画

--- modules/modules_my/src/main/java/com/cl/modules_my/pop/AchievementPop.kt
+++ modules/modules_my/src/main/java/com/cl/modules_my/pop/AchievementPop.kt
@@ -1,74 +1,127 @@
 package com.cl.modules_my.pop
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.MotionEvent
+import android.view.ViewTreeObserver
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.ScaleAnimation
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.cl.common_base.ext.logI
 import com.cl.modules_my.R
 import com.cl.modules_my.databinding.MyPopAchievementBinding
 import com.lxj.xpopup.core.BottomPopupView
+import kotlin.math.max
 
 
-class AchievementPop(context: Context, private val url: String?, private val backUrl: String?, private val title: String?, private val selectStatus: Boolean?, private val isGain: Boolean,private val addAction: (()->Unit)? = null ): BottomPopupView(context) {
+class AchievementPop(
+    context: Context,
+    private val url: String?,
+    private val backUrl: String?,
+    private val title: String?,
+    private val selectStatus: Boolean?,
+    private val isGain: Boolean,
+    private val addAction: (() -> Unit)? = null
+) : BottomPopupView(context) {
 
-        override fun getImplLayoutId(): Int {
-            return R.layout.my_pop_achievement
-        }
+    override fun getImplLayoutId(): Int {
+        return R.layout.my_pop_achievement
+    }
 
-        @SuppressLint("CheckResult")
-        override fun onCreate() {
-            super.onCreate()
-            DataBindingUtil.bind<MyPopAchievementBinding>(popupImplView)?.apply {
-                lifecycleOwner = this@AchievementPop
-                data = backUrl
-                title = this@AchievementPop.title
-                desc = url
-                isSelect = selectStatus ?: false
-                isGain = this@AchievementPop.isGain
+    private lateinit var bind: MyPopAchievementBinding
+    @SuppressLint("CheckResult", "ClickableViewAccessibility")
+    override fun onCreate() {
+        super.onCreate()
+        bind =  DataBindingUtil.bind<MyPopAchievementBinding>(popupImplView)?.apply {
+            lifecycleOwner = this@AchievementPop
+            data = backUrl
+            title = this@AchievementPop.title
+            desc = url
+            isSelect = selectStatus ?: false
+            isGain = this@AchievementPop.isGain
 
-                executePendingBindings()
+            executePendingBindings()
 
-                tvAdd.setOnClickListener {
-                    addAction?.invoke()
-                    dismiss()
-                }
-
-                // isGain
-                val requestOptions = RequestOptions()
-                // 判断是否需要应用灰度转换
-                /*if (!this@AchievementPop.isGain) {
-                    requestOptions.transform(GrayscaleTransformation())
-                }*/
-                // 设置 placeholder 和 error
-                requestOptions.placeholder(com.cl.common_base.R.mipmap.placeholder)
-                requestOptions.error(com.cl.common_base.R.mipmap.errorholder)
-                Glide.with(context)
-                    .load(backUrl)
-                    .apply(requestOptions)
-                    .into(ivAddAccessory)
-
-
-                // 淡入动画
-                // 淡入动画
-                val fadeIn = ObjectAnimator.ofFloat(ivAddAccessory, "alpha", 0f, 1f)
-                fadeIn.setDuration(500)
-
-
-                // 缩放动画
-                val scaleX = ObjectAnimator.ofFloat(ivAddAccessory, "scaleX", 0.5f, 1f)
-                val scaleY = ObjectAnimator.ofFloat(ivAddAccessory, "scaleY", 0.5f, 1f)
-                scaleX.setDuration(500)
-                scaleY.setDuration(500)
-
-                // 组合动画
-                val animatorSet = AnimatorSet()
-                animatorSet.playTogether(fadeIn, scaleX, scaleY)
-                animatorSet.start()
-
-                ivClose.setOnClickListener { dismiss() }
+            tvAdd.setOnClickListener {
+                addAction?.invoke()
+                dismiss()
             }
-        }
+
+            val requestOptions = RequestOptions()
+            // 设置 placeholder 和 error
+            requestOptions.placeholder(com.cl.common_base.R.mipmap.placeholder)
+            requestOptions.error(com.cl.common_base.R.mipmap.errorholder)
+
+            Glide.with(context)
+                .load(backUrl)
+                .apply(requestOptions)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        // 图片加载失败的处理
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable,
+                        model: Any,
+                        target: com.bumptech.glide.request.target.Target<Drawable>,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        if (!this@AchievementPop.isGain) return false
+                        ivAddAccessory.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                            override fun onGlobalLayout() {
+                                // 确保只调用一次
+                                ivAddAccessory.viewTreeObserver.removeOnGlobalLayoutListener(this)
+
+                                // 现在可以安全地使用宽度和高度
+                                val animationSet = AnimationSet(true)
+                                animationSet.duration = 800
+                                animationSet.repeatCount = 0
+
+                                val rotateAnimation = Rotate3dAnimation(-180f, 0f, ivAddAccessory.width / 2.0f, ivAddAccessory.height / 2.0f, 50f, false)
+                                //rotateAnimation.duration = 500 // 初始1秒
+                                rotateAnimation.repeatCount = 0
+
+                                val scaleAnimation = ScaleAnimation(
+                                    0.5f, 1f, // 开始和结束时缩放大小
+                                    0.5f, 1f,
+                                    Animation.RELATIVE_TO_SELF, 0.5f,
+                                    Animation.RELATIVE_TO_SELF, 0.5f
+                                )
+                                //scaleAnimation.duration = 500 // 1秒
+                                rotateAnimation.repeatCount = 0
+
+                                animationSet.addAnimation(rotateAnimation)
+                                animationSet.addAnimation(scaleAnimation)
+
+                                ivAddAccessory.startAnimation(animationSet)
+                            }
+                        })
+                        return false
+                    }
+                })
+                .into(ivAddAccessory)
+
+
+            ivClose.setOnClickListener { dismiss() }
+        }!!
+    }
+
+    override fun onDismiss() {
+        super.onDismiss()
+        bind.ivAddAccessory.clearAnimation()
+    }
 }
