161ad196610cf3ceb915f10bd41e313d670bf846
li <286440803@qq.com>
2024-02-04 16:26:14 +0800

    删除无用easeUI包。

--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -16,7 +16,7 @@ object Version {
     const val applicationId = "com.cl.abby"
     const val minSdk = 26
     const val targetSdk = 33
-    val versionCode = 57
+    val versionCode = 58
     const val versionName = "3.5.2"
 
     const val coreKtxVersion = "1.8.0"
